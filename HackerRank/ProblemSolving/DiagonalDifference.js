b9fc532a667fc41ac3c47e651734ed7a0774f96e
Schraut <danshrout1@gmail.com>
2021-05-05 10:07:22 -0700

    completed diagonal difference challenge.

---
+++ HackerRank/ProblemSolving/DiagonalDifference.js
@@ -0,0 +1,25 @@
+// Diagonal Difference
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+function diagonalDifference(matrix) {
+  const length = matrix.length;
+  let diagonalLeftToRight = 0;
+  let diagonalRightToLeft = 0;
+
+  // Loop through the array to sum up the diagonals.
+  for (let i = 0; i < length; i++) {
+    // left-to-right diagonal
+    diagonalLeftToRight += matrix[i][i];
+    // right-to-left diagonal.
+    diagonalRightToLeft += matrix[length - 1 - i][i];
+  }
+  // return value using Math.abs() to get the absolute difference.
+  return Math.abs(diagonalLeftToRight - diagonalRightToLeft);
+}
+
+diagonalDifference([
+  [1, 2, 3],
+  [4, 5, 6],
+  [9, 8, 9],
+]);
