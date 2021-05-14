ac4f6d677e01ad090e81a08d2d0f1a12ee61170b
Schraut <danshrout1@gmail.com>
2021-05-14 08:49:32 -0700

    completed time-conversion challenge, created reverse-string-manually.js, renamed some files.

---
+++ Algorithms/reverse-string-manually.js
@@ -0,0 +1,13 @@
+// A manual way to reverse a string
+const str = 'abcdefg';
+
+let a = str[0];
+let b = str[1];
+let c = str[2];
+let d = str[3];
+let e = str[4];
+let f = str[5];
+let g = str[6];
+
+let reversedString = g + f + e + d + c + b + a;
+console.log(reversedString);
