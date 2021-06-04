921ca389176838c22b6c98dd4f1956ccecad2ece
Dan Shrout <danshrout1@gmail.com>
2021-06-04 08:01:57 -0700

    completed day 7: Arrays

---
+++ HackerRank/30-days-of-code/day-7-arrays.js
@@ -0,0 +1,9 @@
+// Day 7: Arrays
+// https://www.hackerrank.com/challenges/30-arrays/problem
+
+function reverseArray(arr) {
+  let myInput = arr.reverse();
+  console.log(myInput.join(' '));
+}
+
+reverseArray([1, 4, 3, 2]);
