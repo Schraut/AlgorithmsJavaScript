801427a90313a3f0a11104dcee8b22ab1af27a4d
Dan Shrout <danshrout1@gmail.com>
2021-09-08 09:50:23 -0700

    completed day-8-dictionaries-and-maps

--- HackerRank/30-days-of-code/day-8-dictionaries-and-maps.js
+++ HackerRank/30-days-of-code/day-8-dictionaries-and-maps.js
@@ -1,45 +1,28 @@
 // Day 8: Dictionaries and Maps
 // https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
 
-// TO BE CONTINUED....
-// I was able to create a phonebook and but not finished.
-
 function processData(input) {
   // split the input into an array
-  contactInput = input.split(' ');
+  input = input.split('\n');
   // the first element in the array is a number so this decides how many contacts to create
-  let numberOfEntries = contactInput[0];
-  let phonebook = new Map(); // create a map to store key value pairs
-  //console.log(contactInput);
-  // create a phonebook with a for loop
-  for (let i = 1; i <= numberOfEntries * 2; i += 2) {
-    phonebook.set(contactInput[i], contactInput[i + 1]);
-    //console.log(phonebook);
+  var N = parseInt(input[0]);
+  // create phonebook with an empty array
+  var phonebook = [];
+  // loop through to create contacts of phonebook
+  for (var i = 0; i < N; i++) {
+    var line = input[i + 1];
+    line = line.split(' ');
+    phonebook[line[0]] = line[1];
   }
-  console.log(Array.from(phonebook));
-  // phonebook.forEach(function (value, key) {
-  //   console.log(key + ' = ' + value);
-  // });
-  //console.log(phonebook);
-  // loop through phone book
-  //if (phonebook.has('sam')) console.log(phonebook.get('sam'));
-  // for (let i = 0; i < contactInput.length; i++) {
-  //   if (phonebook.has(contactInput[i]))
-  //     console.log(phonebook.get(contactInput[i]));
-  //   //console.log(phonebook.get(input[i]));
-  //   //else console.log('Not found');
-  //   // if (phonebook.has(contactInput[i])) {
-  //   //   console.log(contactInput[i] + '=' + phonebook[contactInput[i]]);
-  //   // } else {
-  //   //   console.log('Not found');
-  //   // }
-  //   //console.log(phonebook.keys());
-  // }
+
+  for (var i = N + 1; i < input.length; i++) {
+    var num = phonebook[input[i]];
+    if (num !== undefined) {
+      console.log(input[i] + '=' + num);
+    } else {
+      console.log('Not found');
+    }
+  }
 }
 
 processData('3 sam 99912222 tom 11122222 harry 12299933 sam edward harry');
-
-// Sudo code
-// first element is a number and that's how many entries there will be
-// make a phonebook the size of the first element
-//
